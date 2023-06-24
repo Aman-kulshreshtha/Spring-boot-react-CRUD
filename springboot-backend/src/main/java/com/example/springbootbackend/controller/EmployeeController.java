8a6f54b8885a906064cc0f675ca72fcda54542cb
Aman Kulshreshtha <aman21102000@gmail.com>
2023-06-25 00:27:47 +0530

    endpoint created to fetch all employees in backend

---
+++ springboot-backend/src/main/java/com/example/springbootbackend/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package com.example.springbootbackend.controller;
+
+import com.example.springbootbackend.model.Employee;
+import com.example.springbootbackend.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    //endpoint to get list of all employees
+    @GetMapping("employees")
+    public List<Employee> getAllEmployee() {
+        return employeeRepository.findAll();
+    }
+
+}
+
